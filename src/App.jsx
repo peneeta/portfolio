556b064d04e910bff05366d5a76efb6ea1df9a45
peneetawojcik <peneetaw@gmail.com>
2022-11-23 17:03:54 -0500

    update hero

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h1>Peneeta's Portfolio Site</h1>
+      <h2>Coming Soon</h2>
+      <br />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
