f0140b57301c74d03e56a8a7df42c32924c49a36
peneetawojcik <peneetaw@gmail.com>
2022-12-20 15:58:53 +0800

    edits to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import peneetaPortrait from './assets/peneeta_portrait.jpg'
 import './App.css'
 
 import Navbar from './components/Navbar';
@@ -10,13 +11,14 @@ function App() {
     <div className="App">
       <Navbar/>
       <div className="hero">
+        <img className="peneeta-portrait square" src={peneetaPortrait} alt="peneeta portrait" />
         <div className="container">
           <div className="text-wrapper">
-            <h1>Hi, my name is Peneeta.</h1>
+            <h1>Hello, my name is <strong>Peneeta.</strong></h1>
           </div>
 
           <div className="anim-wrapper">
-            <p>This portfolio is still under development.</p>
+            <p>I am an undergraduate student at the University of Virginia with a passion for creative front-end web development.</p>
 
           </div>
         </div>
@@ -25,7 +27,8 @@ function App() {
       <div className="section">
         <div className="container">
           <div className="section-text">
-            {/* <p>I am an undergraduate student at the University of Virginia with a passion for creative web development.</p> */}
+            <h1>I study Biomedical Engineering and Data Science.</h1>
+            <p>My career interests lie at the intersection of computer science and biology. I have participated in a diverse set of research experiences, from cancer research to synthetic biology. My goal is to pursue a career in drug discovery or computational modeling.</p>
           </div>
         </div>
       </div>
@@ -33,7 +36,8 @@ function App() {
       <div className="section">
         <div className="container">
           <div className="section-text">
-            {/* <p>I study Biomedical Engineering and Data Science with career interests in computational biomedical research.</p> */}
+            <h1>My Creative Outlet</h1>
+            <p>After designing and implementing my first complete website for the iGEM competition, I realized the influence that good UI/UX design has. I am self-taught and still have a lot to learn, however I find ways to improve my design skills in addition to my studies.</p>
           </div>
         </div>
       </div>
