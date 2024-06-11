5c65f0502ccf8536e3afb84e99ed2346fcb2a340
peneeta <peneetaw@gmail.com>
2024-06-11 15:50:14 -0400

    complete navbar

--- src/App.jsx
+++ src/App.jsx
@@ -20,22 +20,23 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <div id="home" className="hero">
-        <div className="container px-8 flex flex-row justify-center items-center gap-x-10">
-            <div className="portrait-wrapper">
-              <img src={embellishments} alt="" />
-              {/* <img src={portrait} alt="" /> */}
-            </div>
-          <div className="text-wrapper">
-            <div className="headline">
-              <h2>Hello, my name is</h2>
-              <h1>Peneeta</h1> 
-              <div className="subtitle text-base">
-                <p>Masters student and aspiring genomics researcher, interested broadly in computational biology.</p>
-              </div>
-            </div>
+      <div id="home" className="hero flex items-center justify-center flex-row">
+        <div className="container px-8 flex flex-wrap flex-row justify-center items-center gap-x-10">
+          <div className="portrait-wrapper">
+            <img className="embellishment" src={embellishments} alt="" />
+            <img className="portrait" src={portrait} alt="" />
+          </div>
 
-            <div className="icons-wrapper">
+          <div className="text-wrapper">
+              <div className="headline">
+                <h2 className="mb-1.5">Hello, my name is</h2>
+                <h1 className="pb-1.5">Peneeta</h1> 
+                <div className="subtitle text-base">
+                  <p>Masters student and aspiring genomics researcher, interested broadly in computational biology.</p>
+                </div>
+              </div>
+
+            <div className="icons-wrapper flex flex-row justify-start items-center gap-8 mt-5">
               <a href="https://www.linkedin.com/in/peneeta/" target="_blank">
                 <div className="icon linkedin">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
@@ -86,7 +87,7 @@ function App() {
           <ResourcesList/>
       </div>
 
-      <div className="final px-8 section">
+      <div className="final px-8 section mb-20">
         <div className="section-text my-3">
           <h3>Say hi! 👋</h3>
         </div>
